6571ba882372d2c2f309f3241d6b9ec79b7f723f
Nikita Olenichev <2020nikol@mail.ru>
2023-02-05 22:39:15 +0300

    task5

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class Example(QWidget, Ui_Form):
         super().__init__()
         self.setupUi(self)
         self.searchButton.clicked.connect(self.run)
+        self.searchButton2.clicked.connect(self.run2)
         self.label_error.hide()
         self.spn_el = 0.001
 
@@ -71,6 +72,35 @@ class Example(QWidget, Ui_Form):
                 self.map_label.hide()
         except Exception:
             self.label_error.show()
+
+    def run2(self):
+        try:
+            self.object = self.object_input_lineEdit.text()
+            response = requests.get("http://geocode-maps.yandex.ru/1.x/?"
+                                    "apikey=40d1649f-0493-4b70-98ba-98533de7710b&"
+                                    f"geocode={self.object}&format=json")
+            json_response = response.json()
+            toponym = json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+            coords = toponym['Point']['pos']
+            coords = coords.split()
+            map_request = f'http://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}' \
+                          f'&l=map&z=15&size=650,450&spn={self.spn_el},{self.spn_el}' \
+                          f'&pt={coords[0]},{coords[1]},flag'
+            response = requests.get(map_request)
+            if response and response.status_code == 200:
+                mp = 'map.png'
+                with open(mp, 'wb') as file:
+                    file.write(response.content)
+                self.px = QPixmap('map.png')
+                self.map_label.setPixmap(self.px)
+                os.remove(mp)
+                self.map_label.show()
+            else:
+                self.label_error.show()
+                self.map_label.hide()
+        except Exception:
+            self.label_error.show()
+
 
 
 if __name__ == '__main__':
